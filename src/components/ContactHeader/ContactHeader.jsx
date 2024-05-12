092aefd3132a8d5d424bf712a39900982c44ea47
bikeshtamang1 <tamangbikesh34@gmail.com>
2024-05-12 20:19:26 +0545

    first commit

---
+++ src/components/ContactHeader/ContactHeader.jsx
@@ -0,0 +1,13 @@
+import styles from "./ContactHeader.module.css";
+const ContactHeader = () => {
+  return (
+    <div className={`${styles.contact_section}`}>
+      <h1>React</h1>
+      <p>
+      Contact up page using react
+      </p>
+    </div>
+  );
+};
+
+export default ContactHeader;
